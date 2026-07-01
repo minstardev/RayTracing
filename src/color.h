f67cd137e94f00fd17a447e22fdc64418ec9fd8e
Minsung Kwon <minstardev@gmail.com>
2026-07-01 21:57:04 +0900

    Save output as png

--- src/color.h
+++ src/color.h
@@ -2,10 +2,13 @@
 #define COLOR_H
 
 #include "vec3.h"
+#include <cstdint>
 #include <iostream>
+#include <vector>
 
-// [0,1] 범위의 색상 값을 [0,255] 정수 픽셀 값으로 변환하여 출력
-inline void write_color(std::ostream& out, const color& pixel_color) {
+// scale [0,1] to [0,255] (PPM)
+inline void write_color(std::ostream &out, const color &pixel_color)
+{
     double r = pixel_color.x();
     double g = pixel_color.y();
     double b = pixel_color.z();
@@ -15,6 +18,22 @@ inline void write_color(std::ostream& out, const color& pixel_color) {
     int bbyte = int(255.999 * b);
 
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+// scale [0,1] to [0,255] (PNG)
+inline void write_color(std::vector<uint8_t> &buffer, int pixel_index, const color &pixel_color)
+{
+    double r = pixel_color.x();
+    double g = pixel_color.y();
+    double b = pixel_color.z();
+
+    int rbyte = int(255.999 * r);
+    int gbyte = int(255.999 * g);
+    int bbyte = int(255.999 * b);
+
+    buffer[pixel_index * 3 + 0] = static_cast<uint8_t>(rbyte);
+    buffer[pixel_index * 3 + 1] = static_cast<uint8_t>(gbyte);
+    buffer[pixel_index * 3 + 2] = static_cast<uint8_t>(bbyte);
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,42 @@
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
 #include "color.h"
 #include "vec3.h"
 
+#include <cstdint>
 #include <iostream>
+#include <vector>
 
 int main()
 {
+	int image_width = 256;
+	int image_height = 256;
 
-    // image size
-    int image_width = 256;
-    int image_height = 256;
+	std::vector<uint8_t> buffer(image_width * image_height * 3);
 
-    // Render
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+	// render
+	for (int j = 0; j < image_height; j++)
+	{
+		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+		for (int i = 0; i < image_width; i++)
+		{
+			auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.0);
+			int pixel_index = j * image_width + i;
+			write_color(buffer, pixel_index, pixel_color);
+		}
+	}
 
-    for (int j = 0; j < image_height; j++)
-    {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++)
-        {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.0;
+	std::clog << "\rDone.                          \n";
 
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
+	// save as png
+	const char* filename = "./output/image.png";
+	int stride = image_width * 3; // #bytes per row == width * #channels
+	if (!stbi_write_png(filename, image_width, image_height, 3, buffer.data(), stride))
+	{
+		std::cerr << "Failed to save as PNG: " << filename << '\n';
+		return 1;
+	}
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
-        }
-    }
-
-    std::clog << "\rDone.                 \n";
+	std::clog << "Save completed: " << filename << '\n';
 }
