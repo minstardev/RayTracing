1475e2c4df27baa0c541e465a18d276c3fc359a9
Minsung Kwon <minstardev@gmail.com>
2026-07-03 19:54:32 +0900

    add ray & camera

--- src/main.cpp
+++ src/main.cpp
@@ -2,41 +2,77 @@
 #include "stb_image_write.h"
 
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
 #include <cstdint>
 #include <iostream>
 #include <vector>
 
+color ray_color(const ray &r)
+{
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+
 int main()
 {
-	int image_width = 256;
-	int image_height = 256;
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
 
-	std::vector<uint8_t> buffer(image_width * image_height * 3);
+    // Calculate the image height, and ensure that it's at least 1.
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
 
-	// render
-	for (int j = 0; j < image_height; j++)
-	{
-		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-		for (int i = 0; i < image_width; i++)
-		{
-			auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.0);
-			int pixel_index = j * image_width + i;
-			write_color(buffer, pixel_index, pixel_color);
-		}
-	}
+    // Camera
 
-	std::clog << "\rDone.                          \n";
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width) / image_height);
+    auto camera_center = point3(0, 0, 0);
 
-	// save as png
-	const char* filename = "./output/image.png";
-	int stride = image_width * 3; // #bytes per row == width * #channels
-	if (!stbi_write_png(filename, image_width, image_height, 3, buffer.data(), stride))
-	{
-		std::cerr << "Failed to save as PNG: " << filename << '\n';
-		return 1;
-	}
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
 
-	std::clog << "Save completed: " << filename << '\n';
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    // Calculate the location of the upper left pixel.
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    std::vector<uint8_t> buffer(image_width * image_height * 3);
+
+    // render
+    for (int j = 0; j < image_height; j++)
+    {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++)
+        {
+            int pixel_index = j * image_width + i;
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
+            write_color(buffer, pixel_index, pixel_color);
+        }
+    }
+
+    std::clog << "\rDone.                          \n";
+
+    // save as png
+    const char *filename = "./output/image.png";
+    int stride = image_width * 3; // #bytes per row == width * #channels
+    if (!stbi_write_png(filename, image_width, image_height, 3, buffer.data(), stride))
+    {
+        std::cerr << "Failed to save as PNG: " << filename << '\n';
+        return 1;
+    }
+
+    std::clog << "Save completed: " << filename << '\n';
 }
